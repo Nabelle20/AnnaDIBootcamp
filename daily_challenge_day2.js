11eff368ce658f4efa43c548ca7159164aff904e
Nabelle20 <118258754+Nabelle20@users.noreply.github.com>
2022-11-16 16:48:07 +0400

    Add files via upload

---
+++ daily_challenge_day2.js
@@ -0,0 +1,11 @@
+
+//Exercise 1 
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+console.log(fruits);
+fruits.sort();
+fruits.push('kiwi'); 
+fruits.splice(0,1); 
+fruits.reverse();
+console.log('fruits:', fruits)
+
